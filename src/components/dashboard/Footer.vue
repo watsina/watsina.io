299c6234bb9ac5f73563156e2a945ccb118d7b67
geneowak <teugeneowak@gmail.com>
2020-04-25 22:59:13 +0300

    Initial commit

---
+++ src/components/dashboard/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer class="justify-center" color="#292929" height="100">
+    <div class="title font-weight-light grey--text text--lighten-1 text-center">
+      &copy; {{ new Date().getFullYear() }} — Watsina
+    </div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <!-- <app-bar></app-bar> -->
+    <v-content>
+      <hero></hero>
+
+      <about-us></about-us>
+
+      <!-- <contact-us></contact-us> -->
+    </v-content>
+    <Footer />
+  </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Footer from '@/components/dashboard/Footer.vue';
+import AppBar from '@/components/dashboard/AppBar.vue';
+import Hero from '@/components/sections/Hero.vue';
+import AboutUs from '@/components/sections/About.vue';
+import ContactUs from '@/components/sections/ContactUs.vue';
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: 'Home',
+  // eslint-disable-next-line vue/no-unused-components
+  components: { Footer, AppBar, Hero, AboutUs, ContactUs },
 };
 </script>
